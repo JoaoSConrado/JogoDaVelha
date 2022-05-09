cce829bc3961cdbcb047f9a19ecd1f843dc3468a
João Conrado <99970376+JoaoSConrado@users.noreply.github.com>
2022-05-08 23:34:39 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
-import { useEffect } from 'react'
-import './App.css'
+import { useState } from 'react';
+import { useEffect } from 'react';
+import './App.css';
+import swal from 'sweetalert';
+
 
 type Players = "O" | "X"
 
-function App() {
+var placarX = 0;
+var placarO = 0;
+var placarEmpate = 0;
 
+
+swal(" JOGO DA VELHA ", "🥇 GANHA QUEM FIZER 10 PONTOS PRIMEIRO 🥇");
+function App() {
   const [turn, setTurn] = useState<Players>("O")
   const [winner, setWinner] = useState<Players | null>(null)
   const [draw, setDraw] = useState<boolean | null>(null)
@@ -13,7 +20,6 @@ function App() {
   const GameOver = !!winner || !!draw;
 
 
-  console.log(marks);
 
   const getSquares = () => {
     return new Array(9).fill(true);
@@ -55,6 +61,8 @@ function App() {
       [0, 4, 8],
       [2, 4, 6],
 
+
+
     ];
 
     for (const line of victoryLines) {
@@ -68,19 +76,55 @@ function App() {
     }
   };
 
+  if (winner == "X") {
+    placarX++
+  };
+  if (winner == "O") {
+    placarO++
+  };
+  if (draw) {
+    placarEmpate++
+  }
+
+  if (placarX >= 10) {
+    swal({
+      title: "PARABENS!",
+      text: ' 🏆 O JOGADOR "X" VENCEU 🏆',
+      icon: "success",
+    });
+
+    placarX = 0
+    placarO = 0
+    placarEmpate = 0
+  }
+  if (placarO >= 10) {
+    swal({
+      title: "PARABENS!",
+      text: ' 🏆 O JOGADOR "O" VENCEU 🏆',
+      icon: "success",
+
+    });
+    placarX = 0
+    placarO = 0
+    placarEmpate = 0
+  }
+
   const reset = () => {
     setTurn(marks[0] == "O" ? "X" : "O");
     setMarks({});
     setWinner(null);
     setDraw(null);
 
+
   }
+
 
   useEffect(() => {
     const winner = getWinner()
 
     if (winner) {
       setWinner(winner)
+      localStorage.setItem("1", winner);
     } else {
       if (Object.keys(marks).length === 9) {
         setDraw(true)
@@ -89,11 +133,16 @@ function App() {
   }, [marks])
 
 
+
+
+
   return (
+
     <div className="container">
       {winner && <h1 className='winnerMensagem'> <span className={winner}> {winner}</span> Ganhou</h1>}
 
       {draw && <h1>Empate</h1>}
+
 
       {!GameOver && <p className='winnerMensagem'> É a vez de <span className={turn}> {turn}</span></p>}
 
@@ -107,10 +156,28 @@ function App() {
 
       {GameOver && <button onClick={reset}>Jogar Novamente ⟳ </button>}
 
+
+      <div className='localStorage'>
+        {localStorage.getItem("1") && <h5> ULTIMO GANHADOR: <span> {localStorage.getItem("1")}</span> </h5>}
+
+        {<h5> <span className='Pontos'>PONTOS: </span> <span className='colorO'>O:</span> {placarO} | <span className='colorX'>X:</span> {placarX} | <span className='colorEmpate'>E:</span> {placarEmpate}</h5>}
+
+      </div>
+
     </div >
+
 
   )
 
+
+
 }
 
+
 export default App;
+
+
+
+
+
+
